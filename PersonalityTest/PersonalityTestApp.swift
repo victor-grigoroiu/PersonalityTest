d990c5cb72b735e1dd7a4b2071f99a366e5eca1a
Victor Grigoroiu <victor@mvgsoftware.com>
2022-11-21 15:21:40 +0200

    Initial Commit

---
+++ PersonalityTest/PersonalityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalityTestApp.swift
+//  PersonalityTest
+//
+//  Created by Victor Grigoroiu on 21.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonalityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
